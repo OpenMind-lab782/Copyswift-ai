8a3d3c392c945e33252f326513572785f7428aa2
OpenMind-lab782 <fuitfulnesslife@gmail.com>
2026-08-22 21:00:16 +0200

    export native MuPDF document adapter

--- ecosystem_core/document_adapters/__init__.py
+++ ecosystem_core/document_adapters/__init__.py
@@ -3,5 +3,6 @@ CopySwiftAI™ Document Adapter Registry.
 """
 
 from .registry import DocumentAdapterRegistry
+from .native_mupdf_adapter import NativeMuPDFAdapter
 
-__all__ = ["DocumentAdapterRegistry"]
+__all__ = ["DocumentAdapterRegistry", "NativeMuPDFAdapter"]
